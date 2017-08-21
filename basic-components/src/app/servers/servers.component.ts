517866db4852bfe6b087fc4aacf308e7090b2e4a
jmrodriguez <juan-miguel.rodriguez@soprasteria.com>
2017-08-21 09:35:24 +0200

    Other forms of selectors

--- basic-components/src/app/servers/servers.component.ts
+++ basic-components/src/app/servers/servers.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', //Funciona como un selector de CSS, By element
+  //selector: '[app-servers]', //By attribute
+  //selector: '.app-servers', //By class
   //Inline template, multiline `text`
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>`,
-  //templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  //template: `
+  //  <app-server></app-server>
+  //  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  //styleUrls: ['./servers.component.css']
+  styles: [`
+    h3 {
+      color: red;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
 
